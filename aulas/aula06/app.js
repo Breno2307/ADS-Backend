2890156fb03d7bab225dc4193a339b4a035e8d98
Breno <breno.ferreira.candido@gmail.com>
2025-09-18 21:46:17 -0300

    aula06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
+const tarefaRouter = require("./routes/tarefas");
 
 const app = express();
 
@@ -13,5 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
+app.use("/tarefas", tarefaRouter);
 
 module.exports = app;
