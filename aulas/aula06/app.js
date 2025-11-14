213fbd0b36b99d83f67d621b4db18f21f305f8b8
Breno <breno.ferreira.candido@gmail.com>
2025-11-13 21:12:29 -0300

    adição de coisa na aula06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const tarefaRouter = require("./routes/tarefasRouter.js");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:8000/",
+  })
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
